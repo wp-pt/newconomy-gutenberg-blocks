f2f1517b82699776ed22852b926664f05a5150a8
Zorca <vs@zorca.org>
2018-08-31 15:27:34 +0300

    v1.0.1

--- plugin.php
+++ plugin.php
@@ -5,7 +5,7 @@
  * Description: Gutenberg plugin created via create-guten-block.
  * Author: Zorca
  * Author URI: https://zorca.org/
- * Version: 1.0.0
+ * Version: 1.0.1
  * License: GPL2+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  *
